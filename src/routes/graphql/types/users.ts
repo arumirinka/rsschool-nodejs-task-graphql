cc752f0fdb7809c0bc623213af16ea024b3300c1
Irina Stepanova <arumirinka@github.com>
2025-05-26 23:53:48 +0600

    feat: add profile field to user

--- src/routes/graphql/types/users.ts
+++ src/routes/graphql/types/users.ts
@@ -1,4 +1,5 @@
 import { GraphQLFloat, GraphQLObjectType, GraphQLString } from "graphql";
+import { Profile } from "./profiles.js";
 
 export const User = new GraphQLObjectType({
   name: 'User',
@@ -6,5 +7,6 @@ export const User = new GraphQLObjectType({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     balance: { type: GraphQLFloat },
+    profile: { type: Profile },
   },
 });
