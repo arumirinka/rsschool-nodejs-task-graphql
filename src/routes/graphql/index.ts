c7f689497093498b1fdcdbfeeed57b623037d514
Irina Stepanova <arumirinka@github.com>
2025-05-27 00:52:28 +0600

    feat: add validation, depth limit and error handling

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { schema } from './schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -16,6 +17,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const errors = validate(schema, parse(req.body.query), [depthLimit(5)]);
+
+      if (errors.length) {
+        return { errors };
+      }
+
       return await graphql({
         schema,
         source: req.body.query,
